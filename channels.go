8032a0afa03346a2493058543a8cbd1be9ad7792
Rakesh <justdecodeme@hotmail.com>
2018-08-05 00:02:55 +0530

    basic  course done

--- channels.go
+++ channels.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	runtime.GOMAXPROCS(8)
 	ch := make(chan string)
+	doneCh := make(chan bool)
+
 	go abcGen(ch)
 	go printer(ch)
 	println("This comes first!")
@@ -17,9 +19,11 @@ func abcGen(ch chan string) {
 	for l := byte('a'); l <= byte('z'); l++ {
 		ch <- string(l)
 	}
+	close(ch)
 }
-func printer(ch chan string) {
-	for {
-		println(<-ch)
+func printer(ch chan string, doneCh chan bool) {
+	for l := range ch {
+		println(l)
 	}
+	doneCh <- true
 }
